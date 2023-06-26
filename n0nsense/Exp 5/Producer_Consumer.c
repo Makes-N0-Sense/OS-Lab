82f5842f0a95a9ecd905e83aeda1d9152c8d31ca
AbhiRagh <abhiragh0@gmail.com>
2023-06-27 00:18:03 +0530

    Add Producer Consumer Program

---
+++ n0nsense/Exp 5/Producer_Consumer.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int mutex = 1, full = 0 , empty = 10 , x = 0;
+
+void producer(){
+    mutex = 0;
+    full = full+1;
+    empty = empty - 1;
+    x = x+1;
+    mutex = 1;
+}
+
+void consumer(){
+    mutex = 0;
+    full = full - 1;
+    empty = empty + 1;
+    x = x - 1;
+    mutex = 1;
+}
+void main() {
+    int i,ch=0;
+    while(ch!=3){
+        printf("1.Producer\n2.Consumer\n3.Exit\nEnter Choice: ");
+        scanf("%d",&ch);
+        
+        switch(ch){
+            case 1:
+                if(mutex==1 && empty!=0)
+                    producer();
+                else
+                    printf("Buffer Is Full\n");
+                break;
+            case 2:
+                if(mutex==1 && full!=0)
+                    consumer();
+                else
+                    printf("Buffer is Empty\n");
+                break;
+            case 3:
+                printf("Exiting...\n");
+        }
+    }
+}
